bf17cb0fe8b471edaa2f7f8349396266fe5dba84
yeshuoer <xlsapple@outlook.com>
2024-07-21 20:42:31 +0800

    log rpid

--- src/hooks/usePassport.ts
+++ src/hooks/usePassport.ts
@@ -1,12 +1,14 @@
 import { useRef } from "react";
 import { WebauthnSigner } from "@0xpass/webauthn-signer";
 import { Network, Passport } from "@0xpass/passport";
+import { log } from "@/lib/utils";
 
 export function usePassport(scopeId: string) {
   const signerRef = useRef<WebauthnSigner | null>(null);
   const passportRef = useRef<Passport | null>(null);
 
   if (!signerRef.current) {
+    log('rpid is', window.location.hostname)
     signerRef.current = new WebauthnSigner({
       rpId: window.location.hostname!,
       rpName: "0xPass",
