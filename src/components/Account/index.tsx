448f45b749ccea0e03343e2cc25aee6921ae09cf
yeshuoer <xlsapple@outlook.com>
2024-07-21 20:12:55 +0800

    case error fix

---
+++ src/components/Account/index.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { log, auth } from "@/lib/utils"
+import { useEffect, useState } from "react"
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
+
+export function Account() {
+  const [displayName, setDisplayName] = useState('')
+
+  useEffect(() => {
+    const name = auth.all().desUsername?.userDisplayName
+    setDisplayName(name)
+  }, [])
+
+  return (
+    <div className="flex items-center">
+      <p className="mr-2">{displayName}</p>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" alt='avatar' />
+        <AvatarFallback>User</AvatarFallback>
+      </Avatar>
+    </div>
+  )
+}
