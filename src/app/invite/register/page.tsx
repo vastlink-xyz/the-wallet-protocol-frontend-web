c5937e86b7a172fbe9d5d4bfd96c9c68ca9d8902
yeshuoer <zkyeshuoer@gmail.com>
2024-08-26 18:15:47 +0800

    🐞 fix: lint error

--- src/app/invite/register/page.tsx
+++ src/app/invite/register/page.tsx
@@ -225,7 +225,7 @@ export default function Page() {
                 inviteStatus === 'REGISTERED' && (
                   <>
                     <div className="mb-8 text-center">
-                      <h2 className="text-2xl font-bold mb-4">You've received a crypto transfer!</h2>
+                      <h2 className="text-2xl font-bold mb-4">You have received a crypto transfer!</h2>
                       <p className="mb-2">{inviteInfo.fromEmail} sent you</p>
                       <p className="text-3xl font-bold mb-2">{formatEther(BigInt(inviteInfo.amount))} {inviteInfo.token}</p>
                     </div>
