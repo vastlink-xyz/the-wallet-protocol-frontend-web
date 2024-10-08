cdc476be7af26eb769a47d6dda1d1457650ee9f5
Johnny Jiang <johnny.nan.jiang@gmail.com>
2024-10-08 11:30:27 +1100

    - add comment

--- src/app/(main)/marketplace/components/ProductCard.tsx
+++ src/app/(main)/marketplace/components/ProductCard.tsx
@@ -95,6 +95,7 @@ export function ProductCard({
     }
 
     // kyb / kyc
+    // We don't open products this way, as product id will be randomly generated. Please refer to `TODO 2024-10-08-001`
     if (p.category === 'Compliance' && checkPurchaseStatus(p) === 'active') {
       log('p.id', p.id)
       if (p.id === '000009') {
