640e184177a786f71c9213158e30c19fdf11ef25
yeshuoer <zkyeshuoer@gmail.com>
2024-10-31 21:53:46 +0800

    ✨ feat: add nprogress

--- themes/theme-light/src/pages/invite/transfer/page.tsx
+++ themes/theme-light/src/pages/invite/transfer/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { useEffect, useRef, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { log } from "@/lib/utils";
--- themes/theme-light/src/routes/routeConfig.tsx
+++ themes/theme-light/src/routes/routeConfig.tsx
@@ -1,141 +1,168 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
-import {
-  // Auth related
-  AuthPage,
-  VerifyTransactionPage,
-  VerifyRegistrationPage,
-  
-  // Dashboard related
-  Dashboard,
-  TokenPage,
-  
-  // Marketplace related
-  MarketplacePage,
-  SearchResultPage,
-  MultisigSettingPage,
-  FeatureDetailPage,
-
-  // KYB & KYC related
-  KYBProxyPage,
-  KYBSelfPage,
-  ProductFlowKYBStartingPage,
-  ProductFlowKYBEndingPage,
-  ProductKYCPage,
-  
-  // Invite related
-  InviteTransferPage,
-  InviteRegisterPage,
-} from './routes';
-
-// main layout
-import MainLayout from '@/pages/main-layout';
-
-// error
 import ErrorPage from '@/pages/error';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/auth" replace />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/auth",
-    element: <AuthPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/verify-transaction',
-    element: <VerifyTransactionPage />,
-  },
-  {
-    path: '/verify-registration',
-    element: <VerifyRegistrationPage />,
-  },
-  {
-    path: '/',
-    element: <MainLayout />,
+    async lazy() {
+      const { RootLayout } = await import('@/pages/root-layout');
+      return { Component: RootLayout };
+    },
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/dashboard',
-        children: [
-          {
-            index: true,
-            element: <Dashboard />,
-          },
-          {
-            path: ':token',
-            element: <TokenPage />,
-          }
-        ]
+        path: "/",
+        element: <Navigate to="/auth" replace />,
       },
       {
-        path: '/marketplace',
+        path: "/auth",
+        async lazy() {
+          const { default: AuthPage } = await import('@/pages/auth/page');
+          return { Component: AuthPage };
+        },
+      },
+      {
+        path: '/verify-transaction',
+        async lazy() {
+          const { default: VerifyTransactionPage } = await import('@/pages/verify/transaction/page');
+          return { Component: VerifyTransactionPage };
+        },
+      },
+      {
+        path: '/verify-registration',
+        async lazy() {
+          const { default: VerifyRegistrationPage } = await import('@/pages/verify/registration/page');
+          return { Component: VerifyRegistrationPage };
+        },
+      },
+      {
+        path: '/',
+        async lazy() {
+          const { default: MainLayout } = await import('@/pages/main-layout');
+          return { Component: MainLayout };
+        },
+        errorElement: <ErrorPage />,
         children: [
           {
-            index: true,
-            element: <MarketplacePage />,
-          },
-          {
-            path: 'search-result',
-            element: <SearchResultPage />,
-          },
-          {
-            path: 'feature-detail/:productId',
-            element: <FeatureDetailPage />,
-          },
-          {
-            path: 'multisig-setting',
-            element: <MultisigSettingPage />,
-          },
-          {
-            path: 'kyb-proxy',
-            element: <KYBProxyPage />,
-          },
-          {
-            path: 'kyb-self',
-            element: <KYBSelfPage />,
-          },
-          {
-            path: 'kyc',
-            element: <ProductKYCPage />,
-          },
-          {
-            path: 'product-flow',
+            path: '/dashboard',
             children: [
               {
-                path: 'kyb',
+                index: true,
+                async lazy() {
+                  const { default: Dashboard } = await import('@/pages/dashboard/page');
+                  return { Component: Dashboard };
+                },
+              },
+              {
+                path: ':token',
+                async lazy() {
+                  const { default: TokenPage } = await import('@/pages/dashboard/token/page');
+                  return { Component: TokenPage };
+                },
+              }
+            ]
+          },
+          {
+            path: '/marketplace',
+            children: [
+              {
+                index: true,
+                async lazy() {
+                  const { default: MarketplacePage } = await import('@/pages/marketplace/page');
+                  return { Component: MarketplacePage };
+                },
+              },
+              {
+                path: 'search-result',
+                async lazy() {
+                  const { default: SearchResultPage } = await import('@/pages/marketplace/search-result/page');
+                  return { Component: SearchResultPage };
+                },
+              },
+              {
+                path: 'feature-detail/:productId',
+                async lazy() {
+                  const { default: FeatureDetailPage } = await import('@/pages/marketplace/feature-detail/page');
+                  return { Component: FeatureDetailPage };
+                },
+              },
+              {
+                path: 'multisig-setting',
+                async lazy() {
+                  const { default: MultisigSettingPage } = await import('@/pages/marketplace/multisig-setting/page');
+                  return { Component: MultisigSettingPage };
+                },
+              },
+              {
+                path: 'kyb-proxy',
+                async lazy() {
+                  const { default: KYBProxyPage } = await import('@/pages/marketplace/kyb-proxy/page');
+                  return { Component: KYBProxyPage };
+                },
+              },
+              {
+                path: 'kyb-self',
+                async lazy() {
+                  const { default: KYBSelfPage } = await import('@/pages/marketplace/kyb-self/page');
+                  return { Component: KYBSelfPage };
+                },
+              },
+              {
+                path: 'kyc',
+                async lazy() {
+                  const { default: ProductKYCPage } = await import('@/pages/marketplace/kyc/page');
+                  return { Component: ProductKYCPage };
+                },
+              },
+              {
+                path: 'product-flow',
                 children: [
                   {
-                    path: 'starting',
-                    element: <ProductFlowKYBStartingPage />,
-                  },
-                  {
-                    path: 'ending',
-                    element: <ProductFlowKYBEndingPage />,
+                    path: 'kyb',
+                    children: [
+                      {
+                        path: 'starting',
+                        async lazy() {
+                          const { default: ProductFlowKYBStartingPage } = await import('@/pages/marketplace/product-flow/kyb/starting/page');
+                          return { Component: ProductFlowKYBStartingPage };
+                        },
+                      },
+                      {
+                        path: 'ending',
+                        async lazy() {
+                          const { default: ProductFlowKYBEndingPage } = await import('@/pages/marketplace/product-flow/kyb/ending/page');
+                          return { Component: ProductFlowKYBEndingPage };
+                        },
+                      }
+                    ]
                   }
                 ]
               }
             ]
           }
         ]
-      }
-    ]
-  },
-  {
-    path: '/invite',
-    children: [
-      {
-        path: 'transfer',
-        element: <InviteTransferPage />,
       },
       {
-        path: 'register',
-        element: <InviteRegisterPage />,
+        path: '/invite',
+        children: [
+          {
+            path: 'transfer',
+            async lazy() {
+              const { default: InviteTransferPage } = await import('@/pages/invite/transfer/page');
+              return { Component: InviteTransferPage };
+            },
+          },
+          {
+            path: 'register',
+            async lazy() {
+              const { default: InviteRegisterPage } = await import('@/pages/invite/register/page');
+              return { Component: InviteRegisterPage };
+            },
+          }
+        ]
       }
     ]
-  },
+  }
 ]);
 
 export default router;
