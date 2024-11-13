fcc16613de6402c50a1ff4b2d6d25370a91c9b6b
yeshuoer <zkyeshuoer@gmail.com>
2024-11-13 14:54:26 +0800

    ✨ feat: add guide component

---
+++ themes/theme-light/src/pages/dashboard/components/Guide/index.tsx
@@ -0,0 +1,87 @@
+import { cn } from "@/lib/utils";
+
+function GuideContainer({
+  className,
+  icon,
+  title,
+  description,
+}: {
+  className?: string
+  icon: string
+  title: string
+  description: string
+}) {
+  return <div className={cn(
+      'border border-[#ebebeb] bg-[#fafafa] rounded-[20px]',
+      'p-[16px] tablet:p-[40px] tablet:pb-[20px]',
+      className,
+    )}>
+      <div className={cn(
+        'block tablet:flex tablet:items-center tablet:justify-between flex-wrap',
+        'tablet:h-[46px] tablet:mb-[30px] tablet:px-[20px]',
+      )}>
+        <div className={cn(
+          'flex items-center',
+          'shrink-0 min-w-fit',
+          'w-[218px]',
+          'mb-[12px] tablet:mb-0'
+        )}>
+          <img
+            className={cn(
+              'w-[27px] tablet:w-[37px]',
+              'mr-[16px] tablet:mr-[12px]',
+            )}
+            src={icon}
+            alt="connect-dapps"
+          />
+          <p className={cn(
+            'text-[#111111] font-bold leading-normal',
+            'text-base tablet:text-2xl',
+          )}>{title}</p>
+        </div>
+
+        <p className={cn(
+          'text-[#828282]',
+          'text-[10px] tablet:text-sm',
+          'leading-3 tablet:leading-none',
+          'w-full tablet:w-[319px] laptop:w-[394px] desktop:w-[587px]',
+          'mb-[10px] tablet:mb-0'
+        )}>
+          {description}
+        </p>
+
+        <div className={cn(
+          'w-fit ml-auto tablet:ml-0 tablet:w-auto'
+        )}>
+          <img
+            className="w-[30px] tablet:w-[44px]"
+            src="/imgs/icons/arrow_right_long.svg"
+            alt="arrow-right"
+          />
+        </div>
+      </div>
+    </div>
+}
+
+export function Guide() {
+  return <div>
+    <p className={cn(
+      'text-[#111111] font-bold leading-tight',
+      'text-2xl tablet:text-[32px]',
+      'mb-[24px] tablet:mb-[40px]',
+    )}>Let’s get started</p>
+
+    <GuideContainer
+      icon="/imgs/icons/connect_dapps.svg"
+      title="Connect dApps"
+      description="Link your wallet to any dApp easily with WalletConnect. Enjoy secure access to DeFi, NFTs, and more—all from your wallet in just a few taps!"
+      className="mb-[24px]"
+    />
+
+    <GuideContainer
+      icon="/imgs/icons/top_up.svg"
+      title="Top up"
+      description="Transition smoothly into the web3 world with our intuitive on-ramp. Fund your wallet easily, access decentralized apps."
+    />
+  </div>
+}
--- themes/theme-light/src/pages/dashboard/page.tsx
+++ themes/theme-light/src/pages/dashboard/page.tsx
@@ -1,10 +1,24 @@
+import { useTotalAsset } from "@/hooks/useTotalAsset";
 import { Overview } from "./components/Overview";
 import { TransactionHistory } from "./components/TransactionHistory";
+import { auth } from "@/lib/utils";
+import { Guide } from "./components/Guide";
 
 export default function DashboardPage() {
+  const { address } = auth.all()
+  const { data: totalAsset, isFetched } = useTotalAsset({
+    enabled: !!address,
+  })
+
   return (
     <div className="mt-[40px] mobile:mt-[100px]">
-      <Overview />
+      {
+        totalAsset?.isZero ? (
+          <Guide />
+        ) : (
+          <Overview />
+        )
+      }
       <div className="mt-[60px] tablet:mt-[100px]">
         <TransactionHistory />
       </div>
