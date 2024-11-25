c2fee2363ef9af976e1055b209fca8116e1e2770
yeshuoer <zkyeshuoer@gmail.com>
2024-11-25 16:42:24 +0800

    🌈 style: update viewport meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta 
+          name="viewport" 
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-VJD93GF9ZC"
           strategy="afterInteractive"
