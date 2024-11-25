ba324b96a2774d27371e6d4a5b59dc5fc6efb848
yeshuoer <zkyeshuoer@gmail.com>
2024-11-25 17:13:18 +0800

    🐞 fix: viewport not working error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Script from "next/script";
@@ -17,12 +17,13 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Vastlink",
   description: "Vastlink",
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
-  },
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
