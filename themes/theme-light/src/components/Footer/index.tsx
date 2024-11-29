3fa53fc196c2f10926d38d84d45ff771b774ab6b
Johnny Jiang <johnny.nan.jiang@gmail.com>
2024-11-29 13:09:00 +1100

    - update link

--- themes/theme-light/src/components/Footer/index.tsx
+++ themes/theme-light/src/components/Footer/index.tsx
@@ -38,7 +38,7 @@ export function Footer() {
     </div>
 
     <a 
-      href="https://t.me/+wk6Lw40M2qtlMjk9" 
+      href="https://forms.gle/piGCLqFSyMoJq1sd9" 
       target="_blank" 
       rel="noopener noreferrer" 
       className="text-center text-white text-xl underline leading-9 block mb-[32px] cursor-pointer"
