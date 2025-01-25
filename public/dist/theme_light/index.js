c9a4e4d4a4e4b58860abbc02ea844c0dbbff723a
yeshuoer <zkyeshuoer@gmail.com>
2025-01-25 15:44:36 +0800

    🔧 build: light theme

--- public/dist/theme_light/index.js
+++ public/dist/theme_light/index.js
@@ -1,5 +1,5 @@
-import "./vendor-radix-AvjOLIap.js";
-import { b6 as n } from "./main-93vrANEB.js";
+import "./vendor-radix-BYJ0AyiX.js";
+import { b8 as n } from "./main-CTEjG-UW.js";
 export {
   n as mountFunction
 };
