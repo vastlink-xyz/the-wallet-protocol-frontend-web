831df3d9b0ccf030ecdaa1bf8478ac76f7a90cc8
yeshuoer <zkyeshuoer@gmail.com>
2025-03-25 19:55:21 +0800

    🔧 build: light theme

---
+++ public/dist/theme_light/index-DwkFPqIC.js
@@ -0,0 +1,12 @@
+import { j as p } from "./vendor-radix-BYJ0AyiX.js";
+import { c as a } from "./main-DVFfqeHd.js";
+function s({ children: t, className: o }) {
+  return /* @__PURE__ */ p.jsx("div", { className: a([
+    "desktop:w-[1224px] laptop:w-[936px] tablet:w-[720px] w-[346px]",
+    "mx-auto pb-12",
+    o
+  ]), children: t });
+}
+export {
+  s as C
+};
--- public/dist/theme_light/index.js
+++ public/dist/theme_light/index.js
@@ -1,5 +1,5 @@
 import "./vendor-radix-BYJ0AyiX.js";
-import { b8 as n } from "./main-Cs8rq8oH.js";
+import { b8 as n } from "./main-DVFfqeHd.js";
 export {
   n as mountFunction
 };
