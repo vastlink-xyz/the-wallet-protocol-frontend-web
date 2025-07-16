1095fb68619a6ed15a259f24ab43f53dca916df9
Kevin Zhou <kevinzhou@Kevins-MacBook-Pro.local>
2025-07-16 22:32:07 +0800

    change defaultLocale to en

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,4 +1,4 @@
 export type Locale = (typeof locales)[number];
 
 export const locales = ['en', 'zh'] as const;
-export const defaultLocale: Locale = 'zh';
+export const defaultLocale: Locale = 'en';
