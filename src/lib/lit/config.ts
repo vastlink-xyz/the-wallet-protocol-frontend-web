32a4c3e67bc5babc8ace5724d73068bf284dc438
yeshuoer <zkyeshuoer@gmail.com>
2025-08-18 10:54:38 +0800

    ✨ feat: update selected Lit network configuration to use environment variable

--- src/lib/lit/config.ts
+++ src/lib/lit/config.ts
@@ -1,10 +1,11 @@
 import { LIT_NETWORK } from '@lit-protocol/constants';
+import { LIT_NETWORKS_KEYS } from '@lit-protocol/types';
 
 // Basic configuration
 // export const DOMAIN = 'localhost';
 // export const ORIGIN = `http://${DOMAIN}:3000`;
 const ORIGIN = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'
-export const SELECTED_LIT_NETWORK = LIT_NETWORK.DatilDev;
+export const SELECTED_LIT_NETWORK = process.env.NEXT_PUBLIC_LIT_NETWORK as LIT_NETWORKS_KEYS;
 
 // Auth redirect URLs
 export const GOOGLE_SIGNIN_REDIRECT = ORIGIN + '/auth/google-callback';
