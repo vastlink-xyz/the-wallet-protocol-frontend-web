d91d2798b7ec7745ad75845a9cf6f515a25bdc7b
Johnny Jiang <johnny.nan.jiang@gmail.com>
2025-09-20 09:43:49 +1000

    - refactor tests

--- tests/wallet.test.ts
+++ tests/wallet.test.ts
@@ -6,9 +6,9 @@ import { LitNodeClient } from '@lit-protocol/lit-node-client';
 import { ExecuteTransactionParams, executePersonalTransaction } from '@/services/personalTransactionService';
 import { AuthProviderType } from '@/lib/lit/custom-auth';
 
-const LONG_TIMEOUT = 99999;
+import { theUser, testConfig } from './fixtures';
 
-describe('Wallet tests', () => {
+describe('wallet tests', () => {
   it('should be able to sign and broadcast a BTC transaction', async () => {
     const input: ExecuteTransactionParams = {
       state: {
@@ -19,11 +19,11 @@ describe('Wallet tests', () => {
         mfaMethodId: null,
         mfaPhoneNumber: null,
       },
-      accessToken: "eyJhbGciOiJSUzI1NiIsImtpZCI6Imp3ay10ZXN0LTc3ZGJlNTA4LTEyYjAtNDI1Mi05NTIyLWVkNDhjNDJhNjQ0ZCIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsicHJvamVjdC10ZXN0LTBlMjUyYjRkLTQ0MzktNGVjMi1iNTg4LWU3ZDA1MGZkOGIzYiJdLCJleHAiOjE3NTgyNzQyODcsImh0dHBzOi8vc3R5dGNoLmNvbS9zZXNzaW9uIjp7ImlkIjoic2Vzc2lvbi10ZXN0LWNiYjYyOTZjLTU0MDgtNGY4NC1iYjkxLTczODY3OTA4NTM4OCIsInN0YXJ0ZWRfYXQiOiIyMDI1LTA5LTE5VDA5OjI2OjI3WiIsImxhc3RfYWNjZXNzZWRfYXQiOiIyMDI1LTA5LTE5VDA5OjI2OjI3WiIsImV4cGlyZXNfYXQiOiIyMDI1LTA5LTI2VDA5OjI2OjI3WiIsImF0dHJpYnV0ZXMiOnsidXNlcl9hZ2VudCI6IiIsImlwX2FkZHJlc3MiOiIifSwiYXV0aGVudGljYXRpb25fZmFjdG9ycyI6W3sidHlwZSI6Im90cCIsImRlbGl2ZXJ5X21ldGhvZCI6ImVtYWlsIiwibGFzdF9hdXRoZW50aWNhdGVkX2F0IjoiMjAyNS0wOS0xOVQwOToyNjoyN1oiLCJlbWFpbF9mYWN0b3IiOnsiZW1haWxfaWQiOiJlbWFpbC10ZXN0LThmZTEzNWZhLTE5NmMtNDdiZi1iYmJhLTA3MDM4MTIzZTU1MCIsImVtYWlsX2FkZHJlc3MiOiJqb2hubnkubmFuLmppYW5nQGdtYWlsLmNvbSJ9fV19LCJpYXQiOjE3NTgyNzM5ODcsImlzcyI6InN0eXRjaC5jb20vcHJvamVjdC10ZXN0LTBlMjUyYjRkLTQ0MzktNGVjMi1iNTg4LWU3ZDA1MGZkOGIzYiIsIm5iZiI6MTc1ODI3Mzk4Nywic3ViIjoidXNlci10ZXN0LTU4NmVlYjM0LTVjNGUtNDljNS1iNzQ5LTYxNmY4MGFlZjgyMiJ9.CC7dB8TK3y8MNMpo21dWtpQXIQIdVP8D67Lb6d3I5kJI6YeXfuwVp3dAkc4FKjqqB8po0J7bdy0i5QQ_137brTYfKgrhlLqH0vraJ2QCltZQJKJXJHk5x5joFf9UiF87BMwUEh5ysmIusVUoJEGYXhQ9K-pVu995L1XAFxY0bQPPl9gDsB2cofejkAW6lRIol3CkGT-agDqq1NtmK3fGfrUnLhY-zPuRHu1ph21Zf1vjI2FytBDMQ8Iuvprn8WafG6K06hWWnBdCsZwcWei3sudIEDGuRmBXEvlt_AoliSH9vgN3FSIEv4GxYYZkz4vAl_t8PzQEsfiXaPNatE8deQ",
+      accessToken: theUser.accessToken,
       authMethodType: 37246459,
       authMethodId: "0xcd143a7bca60c810ad2180113e82606ca4c551615ca48ef14f15dcd00691424e",
       providerType: "email_otp",
-      userEmail: "johnny.nan.jiang@gmail.com",
+      userEmail: theUser.email,
       litActionPkp: {
         ethAddress: "0x09B0Fd65bFD4D04485431AA23b7da954b9F19E76",
         publicKey: "0x04341f97273b2ea4de16d5ded798aa6e7deb537db2925f3268dc75425cddfed86386ff06d07b2f1a639a5df39c6a39bbb176b1a6b8a70fee7d035edbeac087e32a",
@@ -44,7 +44,7 @@ describe('Wallet tests', () => {
   
     expect(res).toBeDefined();
 
-  }, LONG_TIMEOUT);
+  }, testConfig.LONG_TIMEOUT);
 
   it('should be able to sign and broadcast a ETH transaction', async () => {
     const input: ExecuteTransactionParams = {
@@ -56,11 +56,11 @@ describe('Wallet tests', () => {
         mfaMethodId: null,
         mfaPhoneNumber: null,
       },
-      accessToken: "eyJhbGciOiJSUzI1NiIsImtpZCI6Imp3ay10ZXN0LTc3ZGJlNTA4LTEyYjAtNDI1Mi05NTIyLWVkNDhjNDJhNjQ0ZCIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsicHJvamVjdC10ZXN0LTBlMjUyYjRkLTQ0MzktNGVjMi1iNTg4LWU3ZDA1MGZkOGIzYiJdLCJleHAiOjE3NTc3MjkwMzMsImh0dHBzOi8vc3R5dGNoLmNvbS9zZXNzaW9uIjp7ImlkIjoic2Vzc2lvbi10ZXN0LTIyNzM2YTc1LTk0N2ItNGVlNy04MTI3LTg3MTA2ZjYwNTAzZCIsInN0YXJ0ZWRfYXQiOiIyMDI1LTA5LTEzVDAxOjU4OjUyWiIsImxhc3RfYWNjZXNzZWRfYXQiOiIyMDI1LTA5LTEzVDAxOjU4OjUzWiIsImV4cGlyZXNfYXQiOiIyMDI1LTA5LTIwVDAxOjU4OjUyWiIsImF0dHJpYnV0ZXMiOnsidXNlcl9hZ2VudCI6IiIsImlwX2FkZHJlc3MiOiIifSwiYXV0aGVudGljYXRpb25fZmFjdG9ycyI6W3sidHlwZSI6Im90cCIsImRlbGl2ZXJ5X21ldGhvZCI6ImVtYWlsIiwibGFzdF9hdXRoZW50aWNhdGVkX2F0IjoiMjAyNS0wOS0xM1QwMTo1ODo1MloiLCJlbWFpbF9mYWN0b3IiOnsiZW1haWxfaWQiOiJlbWFpbC10ZXN0LThmZTEzNWZhLTE5NmMtNDdiZi1iYmJhLTA3MDM4MTIzZTU1MCIsImVtYWlsX2FkZHJlc3MiOiJqb2hubnkubmFuLmppYW5nQGdtYWlsLmNvbSJ9fV19LCJpYXQiOjE3NTc3Mjg3MzMsImlzcyI6InN0eXRjaC5jb20vcHJvamVjdC10ZXN0LTBlMjUyYjRkLTQ0MzktNGVjMi1iNTg4LWU3ZDA1MGZkOGIzYiIsIm5iZiI6MTc1NzcyODczMywic3ViIjoidXNlci10ZXN0LTU4NmVlYjM0LTVjNGUtNDljNS1iNzQ5LTYxNmY4MGFlZjgyMiJ9.y_wzncXxDzD9hRk79xCAQv8tkm8ozXcAsn0fq_05AcvAy3Hidjzbt0aE79ALlsNSDAX8J8AkyzDtqs5or2Kf6r6OOaTN47RJGQ-V8Lu0LU6sAV4JEcZn1thCwLJfVyP5egJwbAm6gbe7QXA09QQNFY9XPM1wfBkXeA-OsdfyFFHTOhrvSPm6l8_WDKOsv561nxMesmxHC_4sod82D2iVgNmCgakmmBnvdnPLT0raHVtYoG3mCO2HxBIkArNRHKlUfAqUM-SbVVZslcm_jG7dlZFBYUXV9b2W8Uc0lggm-AnUTZ5K80AlgLC05lAdTPtRUTUvf_ba7ikd_CLHBwYkIw",
+      accessToken: theUser.accessToken,
       authMethodType: 37246459,
       authMethodId: "0xcd143a7bca60c810ad2180113e82606ca4c551615ca48ef14f15dcd00691424e",
       providerType: "email_otp",
-      userEmail: "johnny.nan.jiang@gmail.com",
+      userEmail: theUser.email,
       litActionPkp: {
         ethAddress: "0x09B0Fd65bFD4D04485431AA23b7da954b9F19E76",
         publicKey: "0x04341f97273b2ea4de16d5ded798aa6e7deb537db2925f3268dc75425cddfed86386ff06d07b2f1a639a5df39c6a39bbb176b1a6b8a70fee7d035edbeac087e32a",
@@ -81,7 +81,7 @@ describe('Wallet tests', () => {
   
     expect(res).toBeDefined();
 
-  }, LONG_TIMEOUT);
+  }, testConfig.LONG_TIMEOUT);
 
   it('should be able to sign and broadcast a USDC transaction', async () => {
     const input: ExecuteTransactionParams = {
@@ -93,11 +93,11 @@ describe('Wallet tests', () => {
         mfaMethodId: null,
         mfaPhoneNumber: null,
       },
-      accessToken: "eyJhbGciOiJSUzI1NiIsImtpZCI6Imp3ay10ZXN0LTc3ZGJlNTA4LTEyYjAtNDI1Mi05NTIyLWVkNDhjNDJhNjQ0ZCIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsicHJvamVjdC10ZXN0LTBlMjUyYjRkLTQ0MzktNGVjMi1iNTg4LWU3ZDA1MGZkOGIzYiJdLCJleHAiOjE3NTc3MjkwMzMsImh0dHBzOi8vc3R5dGNoLmNvbS9zZXNzaW9uIjp7ImlkIjoic2Vzc2lvbi10ZXN0LTIyNzM2YTc1LTk0N2ItNGVlNy04MTI3LTg3MTA2ZjYwNTAzZCIsInN0YXJ0ZWRfYXQiOiIyMDI1LTA5LTEzVDAxOjU4OjUyWiIsImxhc3RfYWNjZXNzZWRfYXQiOiIyMDI1LTA5LTEzVDAxOjU4OjUzWiIsImV4cGlyZXNfYXQiOiIyMDI1LTA5LTIwVDAxOjU4OjUyWiIsImF0dHJpYnV0ZXMiOnsidXNlcl9hZ2VudCI6IiIsImlwX2FkZHJlc3MiOiIifSwiYXV0aGVudGljYXRpb25fZmFjdG9ycyI6W3sidHlwZSI6Im90cCIsImRlbGl2ZXJ5X21ldGhvZCI6ImVtYWlsIiwibGFzdF9hdXRoZW50aWNhdGVkX2F0IjoiMjAyNS0wOS0xM1QwMTo1ODo1MloiLCJlbWFpbF9mYWN0b3IiOnsiZW1haWxfaWQiOiJlbWFpbC10ZXN0LThmZTEzNWZhLTE5NmMtNDdiZi1iYmJhLTA3MDM4MTIzZTU1MCIsImVtYWlsX2FkZHJlc3MiOiJqb2hubnkubmFuLmppYW5nQGdtYWlsLmNvbSJ9fV19LCJpYXQiOjE3NTc3Mjg3MzMsImlzcyI6InN0eXRjaC5jb20vcHJvamVjdC10ZXN0LTBlMjUyYjRkLTQ0MzktNGVjMi1iNTg4LWU3ZDA1MGZkOGIzYiIsIm5iZiI6MTc1NzcyODczMywic3ViIjoidXNlci10ZXN0LTU4NmVlYjM0LTVjNGUtNDljNS1iNzQ5LTYxNmY4MGFlZjgyMiJ9.y_wzncXxDzD9hRk79xCAQv8tkm8ozXcAsn0fq_05AcvAy3Hidjzbt0aE79ALlsNSDAX8J8AkyzDtqs5or2Kf6r6OOaTN47RJGQ-V8Lu0LU6sAV4JEcZn1thCwLJfVyP5egJwbAm6gbe7QXA09QQNFY9XPM1wfBkXeA-OsdfyFFHTOhrvSPm6l8_WDKOsv561nxMesmxHC_4sod82D2iVgNmCgakmmBnvdnPLT0raHVtYoG3mCO2HxBIkArNRHKlUfAqUM-SbVVZslcm_jG7dlZFBYUXV9b2W8Uc0lggm-AnUTZ5K80AlgLC05lAdTPtRUTUvf_ba7ikd_CLHBwYkIw",
+      accessToken: theUser.accessToken,
       authMethodType: 37246459,
       authMethodId: "0xcd143a7bca60c810ad2180113e82606ca4c551615ca48ef14f15dcd00691424e",
       providerType: "email_otp",
-      userEmail: "johnny.nan.jiang@gmail.com",
+      userEmail: theUser.email,
       litActionPkp: {
         ethAddress: "0x09B0Fd65bFD4D04485431AA23b7da954b9F19E76",
         publicKey: "0x04341f97273b2ea4de16d5ded798aa6e7deb537db2925f3268dc75425cddfed86386ff06d07b2f1a639a5df39c6a39bbb176b1a6b8a70fee7d035edbeac087e32a",
@@ -118,6 +118,6 @@ describe('Wallet tests', () => {
   
     expect(res).toBeDefined();
 
-  }, LONG_TIMEOUT);
+  }, testConfig.LONG_TIMEOUT);
 });
 
